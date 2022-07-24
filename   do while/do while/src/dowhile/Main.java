45f48e24b7ee94a8505b8fe58f71cd3ad8482597
wilfredo <109392939+wilfrevarca@users.noreply.github.com>
2022-07-24 17:48:22 -0500

    Add files via upload

---
+++   do while/do while/src/dowhile/Main.java
@@ -0,0 +1,20 @@
+    package dowhile;
+
+
+    public class Main {
+
+        public static void main(String[] args) {
+            int numero = 0;
+     do {
+
+
+         System.out.println(numero);
+         numero = numero +3;
+
+     }  while (numero<+1);
+
+
+
+
+        }
+    }
